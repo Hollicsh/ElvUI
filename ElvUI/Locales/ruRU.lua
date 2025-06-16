4b9d22a3be3a8be4bb80404b350f5358edc4ebdf
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-06-16 16:41:52 +0500

    Update ruRU.lua

--- ElvUI/Locales/ruRU.lua
+++ ElvUI/Locales/ruRU.lua
@@ -474,6 +474,7 @@ L["EHELP_COMMANDS"] = ([=[Ниже представлен список всех 
  */egrid|r ^64|r или ^128|r или ^256|r  -  Вкл/выкл экранную сетку.
  */edebug|r ^on|r или ^off|r  -  Отключить все аддоны, кроме ElvUI.
   ВАЖНО: */edebug|r ^off|r включит обратно все отключенные В ДАННОЙ СЕССИИ (без /rl) командой */edebug|r ^on|r аддоны.
- */guildlist|r ^minPlayers|r  -  Dumps the sorted list of guilds from the finder.
- */guildapply|r ^"message"|r  -  Applies to the top 5 new guilds with an optional message.
+ */guildlist|r ^minPlayers|r  -  Выводит отсортированный список гильдий из поисковика.
+ */guildapply|r ^"message"|r  -  Применяется к пяти новым гильдиям с дополнительным сообщением.
+
 ]=]):gsub('*', E.InfoColor):gsub('%^', E.InfoColor2)
